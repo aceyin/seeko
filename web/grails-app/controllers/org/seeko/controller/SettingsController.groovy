009b2bc9d8d3d39622d9dab08fbe9accac3c03f1
Ace Yin <aceyin@gmail.com>
2013-09-13 23:46:18 +0800

    on-going change password

--- web/grails-app/controllers/org/seeko/controller/SettingsController.groovy
+++ web/grails-app/controllers/org/seeko/controller/SettingsController.groovy
@@ -1,14 +1,43 @@
 package org.seeko.controller
 
+import org.apache.commons.lang.StringUtils
+import org.seeko.service.SettingService
+
+import javax.servlet.http.HttpServletRequest
+
 class SettingsController {
 
-    static allowedMethods = [password: ['POST','GET']]
+    static final String GET = 'GET';
+    static final String POST = 'POST';
+
+    SettingService settingService
+
+
+    static allowedMethods = [password: ['POST', 'GET']]
 
     def index() {
-        render (view: "/settings/index")
+        render(view: "/settings/index")
     }
 
+    /**
+     * update/initialize password
+     * @return
+     */
     def password() {
-        render (view: "/settings/password")
+        switch (request.method) {
+            case GET:
+                render(view: "/settings/password")
+                break
+            case POST:
+                savePassword(request);
+                break
+        }
+    }
+
+    private void savePassword(HttpServletRequest request) {
+        String password = request.getParameter('password')
+        String password2 = request.getParameter('password2')
+        if (StringUtils.isBlank(password) || StringUtils.isBlank(password2) || !(password == password2)) {
+        }
     }
 }
--- web/grails-app/services/org/seeko/service/SettingService.groovy
+++ web/grails-app/services/org/seeko/service/SettingService.groovy
@@ -1,12 +1,35 @@
 package org.seeko.service
 
+import org.codehaus.groovy.grails.plugins.codecs.MD5Codec
 import org.seeko.domain.Settings
+import sun.security.provider.MD5
 
 class SettingService {
 
     def grailsApplication
+    private String passwordKey = grailsApplication.config.seeko.settings.keys["admin.password"]
 
+    /**
+     * Get the password of administrator account.
+     * @return
+     */
     def getAdminPassword() {
-        Settings.findWhere(["key":grailsApplication.config.seeko.settings.keys["admin.password"]])
+        Settings.findWhere(["key":passwordKey])
+    }
+
+    /**
+     * Save the password of administrator account.
+     * @param password
+     * @return
+     */
+    def savePassword (String password) {
+        String md5 = MD5Codec.encode (password)
+        Settings settings = getAdminPassword();
+        if (settings == null) {
+            settings = new Settings();
+            settings.setKey(passwordKey)
+        }
+        settings.setValue(md5)
+        settings.save()
     }
 }
