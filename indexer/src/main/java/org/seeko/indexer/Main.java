474d6f8b992be364ca2192659a3b0b6ab537a2d0
aceyin <aceyin@gmail.com>
2013-10-11 20:14:47 +0800

    update project configuration

---
+++ indexer/src/main/java/org/seeko/indexer/Main.java
@@ -0,0 +1,11 @@
+package org.seeko.indexer;
+
+/**
+ * The main class for indexer module, used to startup this module.
+ * Created by eniayin on 13-10-11.
+ */
+public class Main {
+    public static void main(String... args) {
+
+    }
+}
