4f6ce1aa6b280ea559aea9634aeeaae488da267c
Ace Yin <aceyin@gmail.com>
2013-11-20 23:24:57 +0800

    remove all grails files

--- indexer/grails-app/services/org/seeko/indexer/IndexSchedulerService.groovy
+++
@@ -1,8 +0,0 @@
-package org.seeko.indexer
-
-class IndexSchedulerService {
-
-    def serviceMethod() {
-
-    }
-}
--- plugins/models/grails-app/domain/org/seeko/Settings.groovy
+++
@@ -1,7 +0,0 @@
-package org.seeko
-
-class Settings {
-
-    static constraints = {
-    }
-}
