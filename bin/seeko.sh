0cea770e59c7e55fe57a446579aa1dc6d0f8bcde
Ace Yin <aceyin@gmail.com>
2013-11-30 14:24:35 +0800

    adjust project structure add shell script

--- bin/seeko.sh
+++ bin/seeko.sh
@@ -1,12 +1,45 @@
 #!/bin/sh
-#### Check Java
+#
+# System configurations
+#
+# The HTTP port of the seeko web module. Default is 8080
+HTTP_PORT=8080
+#
+# The heap size of the seeko web module. Default is 512m.
+HEAP_SIZE=512m
+#
+# The heap size for indexer module. Default is 1024m.
+# The core functions of indexer module are:
+# 1. checkout/clone the source codes from the remote repositories.
+# 2. read and parse the source codes and submit the content to the elasticsearch module.
+# 3. check the changes of the remote repositories regularly and submit the changes into elasticsearch module.
+# Since the indexer module need to read and parse files, so the memory size will impact the performance of this module.
+# Please set the heap size of indexer module to the corresponding value accordingly.
+INDEXER_HEAP_SIZE=1024m
+#
+# The heap size of elasticsearch module. Default is 1024m.
+# The elasticsearch module is the core module provides the full text search feature.
+# The memory size will impact the performance of elasticsearch module.
+# Please set the heap size of elasticsearch module to the corresponding value accordingly.
+ES_HEAP_SIZE=1024m
+#
+# check Java
 JAVA=`which java`
 if [[ 'x'${JAVA} == 'x' ]]; then
-    echo "No Java found in system, please install Java first.";
+    echo "No Java found in system path, please install Java first.";
     exit 1;
 fi
 
-#### Check the SEEKO_HOME
+
+# check the SEEKO_HOME
+if [[ 'x'${SEEKO_HOME} == 'x' ]]; then
+    SEEKO_HOME="`dirname $0`";
+fi
+
+# import the variables in conf
+source "${SEEKO_HOME}/seeko.conf"
+
+# Check the SEEKO_DATA
 USER_HOME="${HOME}";
 if [[ 'x'${USER_HOME} == 'x' ]]; then
     USER_HOME="eval echo ~${USER}";
@@ -14,21 +47,15 @@ if [[ 'x'${USER_HOME} == 'x' ]]; then
         USER_HOME="`ls -d ~`";
     fi
 fi
-SEEKO_HOME="${USER_HOME}/.seeko";
+SEEKO_DATA="${USER_HOME}/.seeko";
 
-#### Create directory if not exist
-if [[ ! -d ${SEEKO_HOME} ]]; then
-    `mkdir ${SEEKO_HOME}`
-fi
+# Main CLI Jar
+CLI="${SEEKO_HOME}/lib/cli.jar";
 
+# Set classpath
+CLASSPATH=".:${CLASSPATH}:${SEEKO_HOME}/lib/cli.jar";
 
-#### Main CLI Jar
-CLI="${PWD}/lib/cli.jar";
-
-#### Set classpath
-CLASSPATH=".:${CLASSPATH}:${PWD}/lib/cli.jar:${PWD}/lib/args4j-2.0.26.jar";
-
-#### JAVA_OPT
-JAVA_OPT="-D SEEKO_HOME=${SEEKO_HOME}"
+# JAVA_OPT
+JAVA_OPT="-D SEEKO_HOME=${SEEKO_HOME} -D SEEKO_DATA=${SEEKO_DATA}"
 
 `${JAVA} -jar ${CLI} -cp ${CLASSPATH} ${JAVA_OPT}`
--- cli/src/main/java/seeko/cli/Main.java
+++ cli/src/main/java/seeko/cli/Main.java
@@ -1,53 +1,12 @@
 package seeko.cli;
 
-import org.kohsuke.args4j.Argument;
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by aceyin on 13-11-21.
  */
 public class Main {
-
-    @Option(name = "-h", usage = "print help message")
-    private String help;
-
-    @Argument
-    private List<String> arguments = new ArrayList<String>();
-
     public static void main(String... args) {
-        System.out.printf("%80s", "Welcome to Seeko");
-        Main main = new Main();
-        main.exec(args);
-    }
-
-    private void exec(String[] args) {
-        CmdLineParser parser = new CmdLineParser(this);
-        parser.setUsageWidth(80);
-
-        try {
-            parser.parseArgument(args);
-            if (arguments.isEmpty()) {
-                throw new CmdLineException(parser, "No arguments");
-            }
-
-
-        } catch (CmdLineException e) {
-            System.err.println(e.getMessage());
-            System.err.println("seeko.sh [options...] arguments...");
-            // print the list of available options
-            parser.printUsage(System.err);
-            System.err.println();
-
-            return;
-        }
-    }
-
-    private void printUsage() {
-        System.out.println("Usage: seeko.sh <install|start|stop>");
+        System.out.println("Trying to get FILE_FILTERS: ");
+        System.out.println(System.getenv("FILE_FILTERS"));
+        System.out.println(System.getProperty("FILE_FILTERS"));
     }
 }
