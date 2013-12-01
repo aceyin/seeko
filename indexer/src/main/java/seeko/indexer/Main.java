c320ad16602b53886675ba7a90854e225a480d34
Ace Yin <aceyin@gmail.com>
2013-12-02 07:47:07 +0800

    add quartz

--- indexer/src/main/java/seeko/indexer/Main.java
+++ indexer/src/main/java/seeko/indexer/Main.java
@@ -6,16 +6,24 @@ import seeko.indexer.scheduler.impl.RepositoryUpdateScheduler;
 /**
  * Created by aceyin on 13-11-30.
  * This is the main class of indexer module response for start the index module.
+ * The main responsibilities of this class are:
+ * 1. startup the indexer node
+ * 2. startup the schedulers
+ * 3. add shutdown hook to stop the schedulers
  */
 public class Main {
     public static void main(String... args) {
-        new RepositoryCheckoutScheduler().start(null);
-        new RepositoryUpdateScheduler().start(null);
+        final RepositoryCheckoutScheduler checkoutScheduler = new RepositoryCheckoutScheduler();
+        checkoutScheduler.start(null);
+
+        final RepositoryUpdateScheduler updateScheduler = new RepositoryUpdateScheduler();
+        updateScheduler.start(null);
 
         Runtime.getRuntime().addShutdownHook(new Thread("Indexer-Shutdown-Hooker-Thread") {
             @Override
             public void run() {
-                throw new RuntimeException("not implement yet");
+                checkoutScheduler.stop(null);
+                updateScheduler.stop(null);
             }
         });
     }
---
+++ indexer/src/main/java/seeko/indexer/job/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by aceyin on 13-12-2.
+ */
+package seeko.indexer.job;
